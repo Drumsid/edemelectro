2168a7346a86cb7a40faabe6fd149f1947bff493
Denis <den-sidnay@yandex.ru>
2019-03-23 14:07:14 +0300

    enf of second step loop last product

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -40,7 +40,7 @@
 				<?php 
 				$args = array(
 					'post_type' => 'product',
-					'post_per_page' => 9,
+					'post_per_page' => 6,
 					// 'meta_key' => '_featured',
 					// 'meta_value' => 'yes'
 				);
@@ -52,85 +52,18 @@
 				if($wp_query->have_posts()) :
 				?>
 
-				<div class="products-grid">
-					<header>
-						<h3 class="head text-center"><?php woocommerce_page_title(); ?></h3>
-					</header>
+			<?php woocommerce_product_loop_start(); ?>
+
+			<?php while($wp_query->have_posts()) : $wp_query->the_post();?>
+
+				<?php wc_get_template_part('content', 'product'); ?>
+
+			<?php endwhile; ?>
 				
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p1.jpg" alt="" /></a>
-							<div class="mask">
-								<a href="single.html">Quick View</a>
-							</div>
-						<a class="product_name" href="single.html">Sed ut perspiciatis</a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$329</span></a></p>
-					</div>
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p2.jpg" alt="" /></a>
-							<div class="mask">
-								<a href="single.html">Quick View</a>
-							</div>
-						<a class="product_name" href="single.html">great explorer</a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$599.8</span></a></p>
-					</div>
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p3.jpg" alt="" /></a>
-							<div class="mask">
-								<a href="single.html">Quick View</a>
-							</div>
-						<a class="product_name" href="single.html">similique sunt</a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$359.6</span></a></p>
-					</div>
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p4.jpg" alt="" /></a>
-							<div class="mask">
-								<a href="single.html">Quick View</a>
-							</div>
-						<a class="product_name" href="single.html">shrinking </a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$649.99</span></a></p>
-					</div>
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p5.jpg" alt="" /></a>
-						<div class="mask">
-							<a href="single.html">Quick View</a>
-						</div>
-						<a class="product_name" href="single.html">perfectly simple</a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$750</span></a></p>
-					</div>
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p6.jpg" alt="" /></a>
-						<div class="mask">
-							<a href="single.html">Quick View</a>
-						</div>
-						<a class="product_name" href="single.html">equal blame</a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$295.59</span></a></p>
-					</div>
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p7.jpg" alt="" /></a>
-						<div class="mask">
-							<a href="single.html">Quick View</a>
-						</div>
-						<a class="product_name" href="single.html">Neque porro</a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$380</span></a></p>
-					</div>
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p8.jpg" alt="" /></a>
-						<div class="mask">
-							<a href="single.html">Quick View</a>
-						</div>
-						<a class="product_name" href="single.html">perfectly simple</a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$540.6</span></a></p>
-					</div>
-					<div class="col-md-4 product simpleCart_shelfItem text-center">
-						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p9.jpg" alt="" /></a>
-						<div class="mask">
-							<a href="single.html">Quick View</a>
-						</div>
-						<a class="product_name" href="single.html">praising pain</a>
-						<p><a class="item_add" href="#"><i></i> <span class="item_price">$229.5</span></a></p>
-					</div>
-					<div class="clearfix"></div>					
-				</div>
+				
+					<div class="clearfix"></div>	
+					<?php woocommerce_product_loop_end(); ?>				
+				<!-- </div> -->
 				<?php endif; ?>
 
 				<?php do_action('woocommerce_after_main_content'); ?>
---
+++ woocommerce/loop/musor.php
@@ -0,0 +1,79 @@
+<?php while($wp_query->have_posts()) : $wp_query->have_posts();?>
+
+						<?php wc_get_template_part('content', 'product'); ?>
+
+				<?php endwhile; ?>
+
+
+<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p1.jpg" alt="" /></a>
+							<div class="mask">
+								<a href="single.html">Quick View</a>
+							</div>
+						<a class="product_name" href="single.html">Sed ut perspiciatis</a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$329</span></a></p>
+					</div>
+					<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p2.jpg" alt="" /></a>
+							<div class="mask">
+								<a href="single.html">Quick View</a>
+							</div>
+						<a class="product_name" href="single.html">great explorer</a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$599.8</span></a></p>
+					</div>
+					<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p3.jpg" alt="" /></a>
+							<div class="mask">
+								<a href="single.html">Quick View</a>
+							</div>
+						<a class="product_name" href="single.html">similique sunt</a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$359.6</span></a></p>
+					</div>
+					<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p4.jpg" alt="" /></a>
+							<div class="mask">
+								<a href="single.html">Quick View</a>
+							</div>
+						<a class="product_name" href="single.html">shrinking </a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$649.99</span></a></p>
+					</div>
+					<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p5.jpg" alt="" /></a>
+						<div class="mask">
+							<a href="single.html">Quick View</a>
+						</div>
+						<a class="product_name" href="single.html">perfectly simple</a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$750</span></a></p>
+					</div>
+					<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p6.jpg" alt="" /></a>
+						<div class="mask">
+							<a href="single.html">Quick View</a>
+						</div>
+						<a class="product_name" href="single.html">equal blame</a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$295.59</span></a></p>
+					</div>
+					<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p7.jpg" alt="" /></a>
+						<div class="mask">
+							<a href="single.html">Quick View</a>
+						</div>
+						<a class="product_name" href="single.html">Neque porro</a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$380</span></a></p>
+					</div>
+					<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p8.jpg" alt="" /></a>
+						<div class="mask">
+							<a href="single.html">Quick View</a>
+						</div>
+						<a class="product_name" href="single.html">perfectly simple</a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$540.6</span></a></p>
+					</div>
+					<div class="col-md-4 product simpleCart_shelfItem text-center">
+						<a href="single.html"><img src="<?php bloginfo('template_directory'); ?>/images/p9.jpg" alt="" /></a>
+						<div class="mask">
+							<a href="single.html">Quick View</a>
+						</div>
+						<a class="product_name" href="single.html">praising pain</a>
+						<p><a class="item_add" href="#"><i></i> <span class="item_price">$229.5</span></a></p>
+					</div>
