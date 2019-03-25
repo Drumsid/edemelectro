20fc758824cd1b55a7aa59e849cdf6746abe28af
Denis <den-sidnay@yandex.ru>
2019-03-25 15:34:13 +0300

    finished slide bottom

---
+++ sidebar-content-bottom.php
@@ -0,0 +1,6 @@
+<div class="other-products">
+	<div class="container">
+		<?php dynamic_sidebar('content_bottom')?>	
+		<!-- <script type="text/javascript" src="js/jquery.flexisel.js"></script> -->
+	</div>
+</div>
