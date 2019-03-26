9a5a75078b4974995b028c6ee1a0fd6227ed3fdd
Denis <den-sidnay@yandex.ru>
2019-03-26 16:07:11 +0300

    add tags widget

--- sidebar.php
+++ sidebar.php
@@ -2,29 +2,23 @@
 	<?php dynamic_sidebar('left_sidebar'); ?>
 
 				
-				
+				<?php if(function_exists('wp_tag_cloud')) :?>
 				<div class="tags">
 				    	<h4 class="tag_head">Tags Widget</h4>
-				         <ul class="tags_links">
-							<li><a href="#">Kitesurf</a></li>
-							<li><a href="#">Super</a></li>
-							<li><a href="#">Duper</a></li>
-							<li><a href="#">Theme</a></li>
-							<li><a href="#">Men</a></li>
-							<li><a href="#">Women</a></li>
-							<li><a href="#">Equipment</a></li>
-							<li><a href="#">Best</a></li>
-							<li><a href="#">Accessories</a></li>
-							<li><a href="#">Men</a></li>
-							<li><a href="#">Apparel</a></li>
-							<li><a href="#">Super</a></li>
-							<li><a href="#">Duper</a></li>
-							<li><a href="#">Theme</a></li>
-							<li><a href="#">Responsive</a></li>
-					        <li><a href="#">Women</a></li>
-							<li><a href="#">Equipment</a></li>
+				        <ul class="tags_links">
+							 <?php $tags = wp_tag_cloud('smallest=8&largest=22&taxonomy=product_tag&format=array');?>
+							
+							 <?php if(is_array($tags)):?>
+								<?php foreach($tags as $tag) :?>
+									<li><?php echo $tag;?></li>
+								<?php endforeach; ?>
+								 
+							<?php endif;?>
+							
 						</ul>
 					
-				     </div>
+				    </div>
+				<?php endif; ?>
+				
 
 			</div>
